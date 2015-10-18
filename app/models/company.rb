7132603896cf02933d63431df11918ed2adc0556
Matthew Roche <MJRoche@usf.edu>
2015-10-18 18:08:10 -0400

    Added validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ActiveRecord::Base
-  validates :name, :street, :city, :state, :service, presence: true
+  validates :name, :street, :city, :state, :zip, :service, presence: true
   validates :zip, :phone, numericality: {only_integer: true, greater_than: 0}
+  validates :zip, length: { is: 5}
+  validates :phone, length: { is: 7}
   has_many :reviews
+
 end
