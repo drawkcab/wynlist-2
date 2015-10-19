c3f86d9e10d4a6fb99950ead6a0e5dfc01a50600
Matthew Roche <MJRoche@usf.edu>
2015-10-19 00:25:17 -0400

    added landing page

---
+++ app/assets/stylesheets/company.rb
@@ -0,0 +1,13 @@
+class Company < ActiveRecord::Base
+  validates :name, :street, :city, :state, :zip, :service, presence: true
+  validates :zip, :phone, numericality: {only_integer: true, greater_than: 0}
+  validates :zip, length: { is: 5}
+  validates :phone, length: { is: 7}
+  has_many :reviews
+
+  def address
+    s = street.strip
+    street+", "+city+", "+state+" "+zip.to_s
+  end
+
+end
